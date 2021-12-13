41fe56e915fa92afb0a34079c3a308622a100280
ruben vazquez <ruben.vazquez@example.com>
2021-12-13 11:37:32 -0600

    cambio ruben agrega relacion one to many y one to one

--- src/main/java/com/nezttech/kanban/security/repository/DetallePizarraUsuario.java
+++ src/main/java/com/nezttech/kanban/security/repository/DetallePizarraUsuario.java
@@ -6,7 +6,7 @@ package com.nezttech.kanban.security.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.nezttech.kanban.entity.nztUsuarioPizarra;
-import com.nezttech.kanban.entity.nztUsuarios;
+
 
 /**
  * @author rubal
